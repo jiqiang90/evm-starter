98a2fb94abb626ffc494c8fa18cada7eea415c8b
Scott Twiname <skott.twiname@gmail.com>
2021-10-20 16:09:45 +1300

    Add example for moonbeam EVM logs handler

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,33 +1,19 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {StarterEntity} from "../types";
-import {Balance} from "@polkadot/types/interfaces";
+import {Transaction} from "../types";
+import { Interface } from '@ethersproject/abi';
+const erc20abi = require('../../erc20.abi.json');
+import { MoonbeamEvent } from '@subql/contract-processors/dist/moonbeam';
 
+export async function handleMoonriverEvent(event: MoonbeamEvent): Promise<void> {
 
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    let record = new StarterEntity(block.block.header.hash.toString());
-    //Record block number
-    record.field1 = block.block.header.number.toNumber();
+    const record = new Transaction(event.transactionHash);
+
+    const iface = new Interface(erc20abi);
+    const parsedLog = iface.parseLog(event);
+
+    record.value = parsedLog.args.value;
+    record.from = parsedLog.args.from;
+    record.to = parsedLog.args.to;
+    record.contractAddress = event.address;
+
     await record.save();
 }
-
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.extrinsic.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
-}
-
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.field5 = true;
-    await record.save();
-}
-
-
