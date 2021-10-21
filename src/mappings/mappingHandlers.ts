94601c83e80c0ca148b1a79ab2fb4f7b1207c44b
Scott Twiname <skott.twiname@gmail.com>
2021-10-21 14:14:51 +1300

    Add example for call processor

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,7 +1,7 @@
-import {Transaction} from "../types";
+import {Approval, Transaction} from "../types";
 import { Interface } from '@ethersproject/abi';
 const erc20abi = require('../../erc20.abi.json');
-import { MoonbeamEvent } from '@subql/contract-processors/dist/moonbeam';
+import { MoonbeamEvent, MoonbeamCall } from '@subql/contract-processors/dist/moonbeam';
 
 export async function handleMoonriverEvent(event: MoonbeamEvent): Promise<void> {
 
@@ -14,6 +14,21 @@ export async function handleMoonriverEvent(event: MoonbeamEvent): Promise<void> 
     record.from = parsedLog.args.from;
     record.to = parsedLog.args.to;
     record.contractAddress = event.address;
+
+    await record.save();
+}
+
+export async function handleMoonriverCall(event: MoonbeamCall): Promise<void> {
+
+    const record = new Approval(event.hash);
+
+    const iface = new Interface(erc20abi);
+    const parsed = iface.decodeFunctionData(`approve(address,uint256)`, event.data);
+
+    record.owner = event.from;
+    record.value = parsed._value;
+    record.spender = parsed._spender;
+    record.contractAddress = event.to;
 
     await record.save();
 }
