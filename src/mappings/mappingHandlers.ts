b788d5416f446f9d5335e122a4a0a10cbc331658
Scott Twiname <skott.twiname@gmail.com>
2021-10-22 17:39:35 +1300

    Add comments to project yaml, update to move out abi parsing and other changes relating to ds-processor

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,33 +1,28 @@
 import {Approval, Transaction} from "../types";
-import { Interface } from '@ethersproject/abi';
-const erc20abi = require('../../erc20.abi.json');
 import { MoonbeamEvent, MoonbeamCall } from '@subql/contract-processors/dist/moonbeam';
+import { BigNumber } from "ethers";
 
-export async function handleMoonriverEvent(event: MoonbeamEvent): Promise<void> {
+// TODO setup typegen from ABI using typechain/ethers
+type TransferEventArgs = [string, string, BigNumber] & { from: string; to: string; value: BigNumber; };
+type ApproveCallArgs = [string, BigNumber] & { _spender: string; _value: BigNumber; }
 
+export async function handleMoonriverEvent(event: MoonbeamEvent<TransferEventArgs>): Promise<void> {
     const record = new Transaction(event.transactionHash);
 
-    const iface = new Interface(erc20abi);
-    const parsedLog = iface.parseLog(event);
-
-    record.value = parsedLog.args.value;
-    record.from = parsedLog.args.from;
-    record.to = parsedLog.args.to;
+    record.value = event.args.value.toBigInt();
+    record.from = event.args.from;
+    record.to = event.args.to;
     record.contractAddress = event.address;
 
     await record.save();
 }
 
-export async function handleMoonriverCall(event: MoonbeamCall): Promise<void> {
-
+export async function handleMoonriverCall(event: MoonbeamCall<ApproveCallArgs>): Promise<void> {
     const record = new Approval(event.hash);
 
-    const iface = new Interface(erc20abi);
-    const parsed = iface.decodeFunctionData(`approve(address,uint256)`, event.data);
-
     record.owner = event.from;
-    record.value = parsed._value;
-    record.spender = parsed._spender;
+    record.value = event.args._value.toBigInt();
+    record.spender = event.args._spender;
     record.contractAddress = event.to;
 
     await record.save();
